5a3c84d330ab45f50b66dcb03b01283acc131ea8
Daniel Sroka <S00912529@acad.tri-c.edu>
2018-12-14 19:57:29 -0500

    Midterm

---
+++ Lab 6/Lab 6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //p[0],p[1],p[2],and p[3]
+
+        { static void Main (string[] args)
+                {
+                int[] array = new int[12];
+
+                Console.WriteLine("{0}{1,15}", "Index", "Month");
+                { String[] month = new string[12];
+                    month[0] = "January";
+                    month[1] = "Febraury";
+                    month[2] = "March";
+                    month[3] = "April";
+                    month[4] = "May";
+                    month[5] = "June";
+                    month[6] = "July";
+                    month[7] = "August";
+                    month[8] = "September";
+                    month[9] = "October";
+                    month[10] = "November";
+                    month[11] = "December";
+                    {
+                        for (int counter = 0; counter < array.Length; ++counter) ;
+                        Console.WriteLine("{0}{1,15}", array[]);
+                    }
+        
+    }
+}
---
+++ Midterm/Midterm/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///The for loop executes if the condition is true and terminates when the condition is false.The If statement executes if it’s true and skips the statement if it’s false. And the while loop checks the statement to make sure it is true and keeps repeating as long as the expression remains true.
+            {
+                int i = 3;
+                Boolean KeepLooping = true;
+                while (KeepLooping)
+                {
+                    if (i <= 2) KeepLooping = false;
+                    i++;
+                    Console.WriteLine(i);
+                }
+                {
+                    int loop = 2; while (loop < 128)
+                    {
+                        Console.WriteLine
+
+
+
+                            const int ARRAY_LENGTH = 7;
+                        int[] array = new int[ARRAY_LENGTH];
+                        for (int counter = 2; counter < array.Length; ++counter)
+                            array[counter] = 2 * counter;
+
+
+                        for (int counter = 0; counter < array.Length; ++counter)
+                            Console.WriteLine("{0,5}{1,8}", counter, array[counter]);
+
+
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
